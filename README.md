1a832c2fbdfa08ec19e4ce278e1cbec41ce8ad84
Maarten Vermeulen <maartencvermeulen@yahoo.comix>
2024-12-22 18:26:18 +0100

    First setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # todo-list
+
+A simple application in Python to experiment with creating a Python package.
---
+++ src/__main__.py
@@ -0,0 +1,9 @@
+""" Entry point of the application """
+
+
+def run_app():
+    print(f"First step")
+
+
+if __name__ == "__main__":
+    run_app()
