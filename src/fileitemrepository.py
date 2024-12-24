114c5784bcbd4080d29577c34daeb47515878cd4
Maarten Vermeulen <maartencvermeulen@yahoo.comix>
2024-12-24 22:32:41 +0100

    add unique id

--- src/fileitemrepository.py
+++ src/fileitemrepository.py
@@ -8,6 +8,7 @@ class FileItemRepository(AbstractItemRepository):
         self.filename = filename
 
     def add(self, item: Item):
+        item.item_id = self.find_last_id() + 1
         with open(self.filename, "a") as file:
             file.write(item.model_dump_json() + "\n")
 
@@ -20,3 +21,12 @@ class FileItemRepository(AbstractItemRepository):
                     return Item(**data)
                 else:
                     return None
+
+    def find_last_id(self):
+        max_id = -1
+        with open(self.filename, "r") as file:
+            for line in file:
+                data = json.loads(line)
+                if max_id < data["item_id"]:
+                    max_id = data["item_id"]
+        return max_id
--- src/model.py
+++ src/model.py
@@ -1,11 +1,12 @@
 from datetime import date, datetime
+from tkinter import NO
 from pydantic import BaseModel, field_validator
 from typing import Optional
 from dateutil.parser import isoparse
 
 
 class Item(BaseModel):
-    item_id: int
+    item_id: int | None = None
     task: str
     start_date: datetime = datetime.now()
     end_date: datetime | None = None
